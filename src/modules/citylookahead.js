d10060859d6a870e97ec49880332dec99086162b
Daniel O'Brien <daniel@monomania.ie>
2021-09-30 21:02:42 +0100

    update

--- src/modules/citylookahead.js
+++ src/modules/citylookahead.js
@@ -4,11 +4,11 @@ const dataList = document.getElementById('datalist');
 export let cities;
 export let countries;
 
-fetch('./dist/cities.json')
+fetch('./cities.json')
   .then((response) => response.json())
   .then((data) => (cities = data));
 
-fetch('./dist/countries.json')
+fetch('./countries.json')
   .then((response) => response.json())
   .then((data) => (countries = data));
 
