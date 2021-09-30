d5f9f92b2139366db5b5bc479f73dd7250448b05
Daniel O'Brien <daniel@monomania.ie>
2021-09-30 21:14:12 +0100

    testing hardcoding the url for github pages on both fetch() methods

--- src/modules/citylookahead.js
+++ src/modules/citylookahead.js
@@ -4,11 +4,11 @@ const dataList = document.getElementById('datalist');
 export let cities;
 export let countries;
 
-fetch('./weatherapp/cities.json')
+fetch('https://dmonomania.github.io/weatherapp/cities.json')
   .then((response) => response.json())
   .then((data) => (cities = data));
 
-fetch('./weatherapp/countries.json')
+fetch('https://dmonomania.github.io/weatherapp/countries.json')
   .then((response) => response.json())
   .then((data) => (countries = data));
 
